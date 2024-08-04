7d50d87e96541157a5cb0531a38853ded6a829b0
CawanTavares <tavarescawan6@gmail.com>
2024-08-04 20:38:04 -0300

    adding app module

--- stadium/lib/app_module.dart
+++ stadium/lib/app_module.dart
@@ -1,14 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:stadium/autenticacao/autenticacao_modula.dart';
 import 'package:stadium/home/home_module.dart';
+import 'package:stadium/services/firebase_msg_service.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds =>
+      [Bind.lazySingleton((i) => FirebaseMessagingService())];
 
   @override
   List<ModularRoute> get routes => [
         ModuleRoute(
           Modular.initialRoute,
+          module: AutenticacaoModule(),
+          transition: TransitionType.fadeIn,
+        ),
+        ModuleRoute(
+          '/home',
           module: HomeMoudle(),
           transition: TransitionType.fadeIn,
         ),
