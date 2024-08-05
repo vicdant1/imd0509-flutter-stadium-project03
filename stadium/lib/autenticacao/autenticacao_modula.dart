5e2253c8ca9f3ce66e9aa48dd253177fd981edb5
CawanTavares <tavarescawan6@gmail.com>
2024-08-04 22:12:40 -0300

    create auth modula

---
+++ stadium/lib/autenticacao/autenticacao_modula.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:stadium/autenticacao/autenticacao_controller.dart';
+import 'package:stadium/autenticacao/autenticacao_page.dart';
+import 'package:stadium/autenticacao/autenticacao_repository.dart';
+import 'package:stadium/autenticacao/cadastro_page.dart';
+
+class AutenticacaoModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind<AutenticacaoRepository>(
+          (i) => AutenticacaoRepository(),
+        ),
+        Bind(
+          (i) => AutenticacaoController(),
+        ),
+      ];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          Modular.initialRoute,
+          child: (_, args) => const AutenticacaoPage(),
+        ),
+        ChildRoute(
+          '/cadastro',
+          child: (_, args) => const CadastroPage(),
+        ),
+      ];
+}
